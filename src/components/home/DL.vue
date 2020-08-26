4b158915d25a0f24863111697924f077bb9c6f6a
Jaylan Snelson <jaylan210@gmail.com>
2020-08-25 22:19:25 -0400

    finally getting git to work and setting up initial DL components

---
+++ src/components/home/DL.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="dl">
+      <h1>NoDa Bodega</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "DL"
+}
+</script>
+
+<style scoped>
+    .dl {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        background: url("../../assets/images/dl.jpg");
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+
+        height: 100vh;
+    }
+
+    .dl h1 {
+        color: #fff;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <DL />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import DL from '@/components/home/DL.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    DL
+  }
+}
+</script>
